9e9736e7a1af63eb7e579a70bee4c2c83275a2fa
Kevin Heritage <kheritage222@gmail.com>
2021-12-04 10:46:39 +0200

    Read and write to file

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,8 @@
 package main
 
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("my_")
+	cards.print()
+	//cards.saveToFile("my_cards")
 
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
 }
